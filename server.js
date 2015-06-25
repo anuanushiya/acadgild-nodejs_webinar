16fd08b7029d44941cbd6c95af719a288c4f7b39
Nihar Sawant <nihar@expojure.com>
2015-06-25 19:14:25 +0530

    Improvement - Render HTML file when / is requested

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 var http = require('http');
+var fs = require('fs');
 
 var server = http.createServer();
 
 server.on('request', function (request, response){
-  response.writeHead(200, { 'Content-Type' : 'text/plain' });
-  response.end('Hello World');
+  if (request.url === '/') {
+    fs.readFile('index.html', function (err, data) {
+      response.writeHead(200, { 'Content-Type' : 'text/html' });
+      response.end(data);
+    });
+  } else {
+    response.writeHead(404, { 'Content-Type' : 'text/plain' });
+    response.end('404 - Page Not Found');
+  }
 });
 
 server.listen(3000);
