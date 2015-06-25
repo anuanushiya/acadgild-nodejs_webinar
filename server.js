b6b6fb9d7b373a9327de12db82c2f580c39bc4e5
Nihar Sawant <nihar@expojure.com>
2015-06-25 19:22:18 +0530

    Chore - Added documentation for Server Initialization

--- server.js
+++ server.js
@@ -3,7 +3,14 @@ var fs = require('fs');
 
 var server = http.createServer();
 
+/**
+ * Listen to Server's request event determine what to do
+ * @param  {object} request     NodeJS Request Object
+ * @param  {object} response    NodeJS Response Object
+ */
 server.on('request', function (request, response){
+
+  // Depending upon the route load appropriate resources
   if (request.url === '/') {
     fs.readFile('index.html', function (err, data) {
       response.writeHead(200, { 'Content-Type' : 'text/html' });
@@ -13,6 +20,10 @@ server.on('request', function (request, response){
     response.writeHead(404, { 'Content-Type' : 'text/plain' });
     response.end('404 - Page Not Found');
   }
+
+  // Log the URL and Status Code for Debugging
+  console.log(request.url, response.statusCode);
+
 });
 
 server.listen(3000);
